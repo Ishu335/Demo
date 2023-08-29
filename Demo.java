688175803b9dcf01e333f82cd19cffb34cb15de2
Ishu335 <100031125+Ishu335@users.noreply.github.com>
2023-08-29 11:05:46 +0530

    Add files via upload
    SPOS FCFS Code

---
+++ Demo.java
@@ -0,0 +1,58 @@
+
+import java.util.*;
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner S=new Scanner(System.in);
+		System.out.println("\nEnter the Process Count:- ");
+		int process_count=S.nextInt();
+		int process_id[]=new int[process_count];
+		int process_ar[]=new int[process_count];
+		int process_bt[]=new int[process_count];
+		int process_ct[]=new int[process_count];
+		int process_ta[]=new int[process_count];
+		int process_wt[]=new int[process_count];
+//		System.out.print("\n Enter the Values:- ");
+//		for (int i=0;i<=process_count;i++)
+//		{
+//			System.out.println("Enter the Process ID:- ");
+//			process_id[i]=S.nextInt();
+//			System.out.println("Enter the Process Arrival Time:- ");
+//			process_ar[i]=S.nextInt();
+//			System.out.println("Enter the Process Burst Time:- ");
+//			process_bt[i]=S.nextInt();
+//		}
+//		int temp;
+//		for(int i=0;i<=process_count;i++)
+//		{
+//			if(process_ar[i]>process_ar[i+1])
+//			{
+////				Arrivel Time
+//				temp=process_ar[i];
+//				process_ar[i]=process_ar[i+1];
+//				process_ar[i+1]=temp;
+////				Process Id
+//				temp=process_id[i];
+//				process_id[i]=process_id[i+1];
+//				process_id[i+1]=temp;
+//				temp=process_ar[i];
+////				Brust time
+//				process_bt[i]=process_bt[i+1];
+//				process_bt[i+1]=temp;
+//				
+//			}
+//		}
+
+		System.out.println("\n\t| Process Id |\t|Arrival Time |\t|Brust Time | ");
+		for(int i=0;i<=process_count;i++)
+		{
+			
+		}
+		}
+	}
+
+
